9ac955fa89fa047aa11583552dc8867708b2540a
Dennis Dreesen <dennis.dreesen@skyline.be>
2023-09-19 15:14:52 +0200

    Just a change

--- SLC-AS-DDreTestScript_1/SLC-AS-DDreTestScript_1.cs
+++ SLC-AS-DDreTestScript_1/SLC-AS-DDreTestScript_1.cs
@@ -17,7 +17,8 @@ namespace SLC_AS_DDreTestScript_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("Hello World123");
+
 		}
 	}
 }
